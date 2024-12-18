d94457e4debcb539e05d43fa4e0a3dfbf5efd23a
coldcocoa <>
2024-12-18 18:17:28 +0900

    C파일몇개
    ---

---
+++ Chap1/Chap1/HellowWorld.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+void MyFuction() {
+	printf("MyFuction");
+}
+
+int myNum = 5;             // 정수
+float myFloat = 5.99;   // 부동소수점
+char myCh = 'asdf';       // 문자
+
+
+
+
+int main() {
+	// Print variables
+	printf("%d\n", myNum);
+	printf("%f\n", myFloat);
+	printf("%c\n", myCh);
+	return 0;
+}
---
+++ Chap1/Chap1/Scanf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+char firstName[30];
+//int main() {
+//	
+//	printf("Enter your first name: \n");
+//
+//	scanf_s("%s", firstName, 30);
+//
+//	printf("Hello %s.", firstName);
+//	return 0;
+//}
